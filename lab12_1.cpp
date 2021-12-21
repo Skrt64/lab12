6cb7239dc3a972927a336fa01f114e129a51ed53
skrt <farm.shk@gmail.com>
2021-12-21 16:50:23 +0700

    lab12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main(){
+    int gade;
+    string G[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    gade = rand()%8;
+    cout << "You will get " << G[gade] << " in this 261102.";
+    return 0;
+}
+
